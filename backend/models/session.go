b61cd05f14d699b23b349a5462dae1acb2fdc1c0
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-10-12 08:36:31 +0530

    integrating calendar's api: session.go

---
+++ backend/models/session.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Session struct {
+	ID           string    `json:"id"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	ExpiresAt    time.Time `json:"expires_at"`
+}
+
+type RefreshTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+	TokenType   string `json:"token_type"`
+	IDToken     string `json:"id_token"`
+}
