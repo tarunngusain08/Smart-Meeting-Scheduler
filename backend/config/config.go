cd997cdd29b43623166d95204e665ce245cf95f2
tarunngusain08 <prudent.tarun0808@gmail.com>
2025-10-19 07:09:54 +0530

    integrating calendar's api: config.go

--- backend/config/config.go
+++ backend/config/config.go
@@ -12,6 +12,7 @@ import (
 	"github.com/coreos/go-oidc"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/microsoft"
 )
 
 type loggingTransport struct {
@@ -47,6 +48,25 @@ type Config struct {
 	Provider     *oidc.Provider
 	OAuth2Config *oauth2.Config
 	Verifier     *oidc.IDTokenVerifier
+}
+
+// GetAccessToken gets a client credentials access token for application permissions
+func (c *Config) GetAccessToken() (string, error) {
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     c.ClientID,
+		ClientSecret: c.ClientSecret,
+		Endpoint:     microsoft.AzureADEndpoint(c.TenantID),
+	}
+
+	token, err := conf.Exchange(ctx, "",
+		oauth2.SetAuthURLParam("grant_type", "client_credentials"),
+		oauth2.SetAuthURLParam("scope", "https://graph.microsoft.com/.default"))
+	if err != nil {
+		return "", fmt.Errorf("failed to get token: %v", err)
+	}
+
+	return token.AccessToken, nil
 }
 
 func LoadConfig() *Config {
@@ -97,7 +117,7 @@ func LoadConfig() *Config {
 		ClientSecret: clientSecret,
 		RedirectURL:  redirectURI,
 		Endpoint:     endpoint,
-		Scopes:       []string{"openid", "profile", "email", "offline_access", "User.Read", "Calendars.Read"},
+		Scopes:       []string{"openid", "profile", "email", "offline_access", "User.Read", "User.ReadBasic.All", "Calendars.Read"},
 	}
 
 	return &Config{
